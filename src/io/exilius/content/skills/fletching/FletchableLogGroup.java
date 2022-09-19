17edbec5e43bf60a7bfdbf38dcb5181745316579
BuildTools <unconfigured@null.spigotmc.org>
2022-09-18 18:03:10 -0700

    added teak stock in to fletchables

--- src/io/exilius/content/skills/fletching/FletchableLogGroup.java
+++ src/io/exilius/content/skills/fletching/FletchableLogGroup.java
@@ -5,7 +5,7 @@ public enum FletchableLogGroup {
 	MAGIC(FletchableLog.MAGIC_SHORTBOW, FletchableLog.MAGIC_LONGBOW, FletchableLog.MAGIC_SHAFT, FletchableLog.MAGIC_STOCK, FletchableLog.MAGIC_SHIELD),
 	YEW(FletchableLog.YEW_SHORTBOW, FletchableLog.YEW_LONGBOW, FletchableLog.YEW_SHAFT, FletchableLog.YEW_STOCK, FletchableLog.YEW_SHIELD),
 	MAPLE(FletchableLog.MAPLE_SHORTBOW, FletchableLog.MAPLE_LONGBOW, FletchableLog.MAPLE_SHAFT, FletchableLog.MAPLE_STOCK, FletchableLog.MAPLE_SHIELD),
-	TEAK(FletchableLog.STOCK),
+	TEAK(FletchableLog.TEAK_SHAFT, FletchableLog.TEAK_STOCK),
 	WILLOW(FletchableLog.WILLOW_SHORTBOW, FletchableLog.WILLOW_LONGBOW, FletchableLog.WILLOW_SHAFT, FletchableLog.WILLOW_STOCK, FletchableLog.WILLOW_SHIELD),
 	OAK(FletchableLog.OAK_SHORTBOW, FletchableLog.OAK_LONGBOW, FletchableLog.OAK_SHAFT, FletchableLog.OAK_STOCK, FletchableLog.OAK_SHIELD),
 	REDWOOD(FletchableLog.ARROW_SHAFT);
