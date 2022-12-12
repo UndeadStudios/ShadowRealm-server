59ce7f082c709f93242e4a438bceb3bf1cf353f8
sgsrocks <seagod6@yahoo.com>
2022-12-12 11:45:26 -0800

    bond fire fix for levels

--- src/io/exilius/content/objects/BondFIres.java
+++ src/io/exilius/content/objects/BondFIres.java
@@ -7,35 +7,40 @@ import io.exilius.model.cycleevent.CycleEventContainer;
 import io.exilius.model.cycleevent.CycleEventHandler;
 import io.exilius.model.definitions.AnimationLength;
 import io.exilius.model.entity.player.Player;
+import io.exilius.model.items.ItemCacheDefinition;
 
 public class BondFIres {
     public enum logData {
-        LOGS(1511, 40),
-        ACHEY_TREE_LOGS(2862, 40),
-        OAK_LOGS(1521, 60),
-        WILLOW_LOGS(1519, 105),
-        TEAK_LOGS(6333, 105),
-        MAPLE_LOGS(1517, 135),
-        YEW_LOGS(1515, 203),
-        ARCTIC_PINE_LOGS(10810, 135),
-        MAHOGANY_LOGS(6332, 158),
-        MAGIC_LOGS(1513, 304),
-        RED_LOGS(7404, 256),
-        GREEN_LOGS(7405, 256),
-        BLUE_LOGS(7406, 256),
-        WHITE_LOGS(10328,  256),
-        PURPLE_LOGS(10329, 256),
-        REDWOOD_LOGS(19669,  350);
+        LOGS(1511, 1, 40),
+        ACHEY_TREE_LOGS(2862, 1, 40),
+        OAK_LOGS(1521, 15, 60),
+        WILLOW_LOGS(1519, 30, 105),
+        TEAK_LOGS(6333, 35, 105),
+        MAPLE_LOGS(1517, 45, 135),
+        YEW_LOGS(1515, 60, 203),
+        ARCTIC_PINE_LOGS(10810, 45, 135),
+        MAHOGANY_LOGS(6332, 50, 158),
+        MAGIC_LOGS(1513, 75, 304),
+        RED_LOGS(7404, 1, 256),
+        GREEN_LOGS(7405, 1, 256),
+        BLUE_LOGS(7406, 1, 256),
+        WHITE_LOGS(10328, 1, 256),
+        PURPLE_LOGS(10329, 1, 256),
+        REDWOOD_LOGS(19669, 90, 350);
 
-        private int logId, xp;
+        private int logId, levelRequirement, xp;
 
-        private logData(int logId, int xp){
+        private logData(int logId, int levelRequirement, int xp){
             this.logId = logId;
+            this.levelRequirement = levelRequirement;
             this.xp = xp;
 
         }
         public int getLogID(){
             return logId;
+        }
+        public int getLevel(){
+            return levelRequirement;
         }
         public int getXP(){
             return xp;
@@ -47,7 +52,10 @@ public class BondFIres {
     public static void BurnLog(final Player c, int itemId, int objectID) {
         for(final logData g : logData.values()) {
             if(c.getItems().playerHasItem(g.getLogID(), 1)) {
-
+                if (player.playerLevel[11] < g.getLevel()) {
+                    player.sendMessage("You need a firemaking level of at least " + g.getLevel() + " to burn the " + ItemCacheDefinition.forID(itemId).getName().toLowerCase() + ".");
+                    return;
+                }
                 if(itemId == g.getLogID()) {
                     c.playerIsFiremaking = true;
                     CycleEventHandler.getSingleton().addEvent(c, new CycleEvent() {
