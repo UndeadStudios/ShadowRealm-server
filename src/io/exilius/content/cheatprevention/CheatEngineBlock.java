c8ea5916ec3a45048e360b09d65bac23c89f6ce8
sgsrocks <seagod6@yahoo.com>
2023-01-08 11:10:06 -0800

    made it so it wont join admins and up

--- src/io/exilius/content/cheatprevention/CheatEngineBlock.java
+++ src/io/exilius/content/cheatprevention/CheatEngineBlock.java
@@ -2,6 +2,7 @@ package io.exilius.content.cheatprevention;
 
 import io.exilius.model.entity.player.Boundary;
 import io.exilius.model.entity.player.Player;
+import io.exilius.model.entity.player.Right;
 import io.exilius.util.Misc;
 import io.exilius.util.discord.Discord;
 
@@ -16,13 +17,15 @@ public class CheatEngineBlock {
 			Discord.writeServerSyncMessage("[CHEAT ENGINE] " + c.getDisplayName() + " is using a cheat engine for the @red@trading post!");
 			Discord.writeCheatEngineMessage("[CHEAT ENGINE] " + c.getDisplayName() + " is using a cheat engine for the @red@trading post!");
 
-			c.setTeleportToX(2086);
-			c.setTeleportToY(4466);
-			c.heightLevel = 0;
-			c.jailEnd = 214700000;
-			c.sendMessage("You have been jailed for using a cheat engine, please speak to staff.");
-			c.sendMessage("If this was a mistake please recall what exactly you did to get here.");
-			c.getPA().closeAllWindows();
+			if(!c.getRights().isOrInherits(Right.ADMINISTRATOR)) {
+				c.setTeleportToX(2086);
+				c.setTeleportToY(4466);
+				c.heightLevel = 0;
+				c.jailEnd = 214700000;
+				c.sendMessage("You have been jailed for using a cheat engine, please speak to staff.");
+				c.sendMessage("If this was a mistake please recall what exactly you did to get here.");
+				c.getPA().closeAllWindows();
+			}
 			return true;
 		} else {
 			Discord.writeServerSyncMessage("[CHEAT ENGINE] " + c.getDisplayName() + " triggered trading post in edge but no jail.");
@@ -40,13 +43,15 @@ public class CheatEngineBlock {
 				Misc.println("" + c.getDisplayName() + " is trying to use a cheatengine to open the Bank.");
 				Discord.writeServerSyncMessage("[CHEAT ENGINE] " + c.getDisplayName() + " is using a cheat engine for the @red@Bank!");
 				Discord.writeCheatEngineMessage("[CHEAT ENGINE] " + c.getDisplayName() + " is using a cheat engine for the @red@Bank!");
-				c.setTeleportToX(2086);
-				c.setTeleportToY(4466);
-				c.heightLevel = 0;
-				c.jailEnd = 214700000;
-				c.sendMessage("You have been jailed for using a cheat engine, please speak to staff.");
-				c.sendMessage("If this was a mistake please recall what exactly you did to get here.");
-				c.getPA().closeAllWindows();
+				if(!c.getRights().isOrInherits(Right.ADMINISTRATOR)) {
+					c.setTeleportToX(2086);
+					c.setTeleportToY(4466);
+					c.heightLevel = 0;
+					c.jailEnd = 214700000;
+					c.sendMessage("You have been jailed for using a cheat engine, please speak to staff.");
+					c.sendMessage("If this was a mistake please recall what exactly you did to get here.");
+					c.getPA().closeAllWindows();
+				}
 				return true;
 			} else {
 				Discord.writeServerSyncMessage("[CHEAT ENGINE] " + c.getDisplayName() + " triggered trading post in edge but no jail.");
@@ -86,13 +91,15 @@ public class CheatEngineBlock {
 			Misc.println(""+ c.getDisplayName() +" is trying to use a cheatengine to open the donator boxes.");
 			Discord.writeServerSyncMessage("[CHEAT ENGINE] "+ c.getDisplayName() +" is using a cheat engine for the @red@donator boxes!");
 			Discord.writeCheatEngineMessage("[CHEAT ENGINE] " + c.getDisplayName() + " is using a cheat engine for the @red@donator boxes!");
-			c.setTeleportToX(2086);
-			c.setTeleportToY(4466);
-			c.heightLevel = 0;
-			c.jailEnd = 214700000;
-			c.sendMessage("You have been jailed for using a cheat engine, please speak to staff.");
-			c.sendMessage("If this was a mistake please recall what exactly you did to get here.");
-			c.getPA().closeAllWindows();
+			if(!c.getRights().isOrInherits(Right.ADMINISTRATOR)) {
+				c.setTeleportToX(2086);
+				c.setTeleportToY(4466);
+				c.heightLevel = 0;
+				c.jailEnd = 214700000;
+				c.sendMessage("You have been jailed for using a cheat engine, please speak to staff.");
+				c.sendMessage("If this was a mistake please recall what exactly you did to get here.");
+				c.getPA().closeAllWindows();
+			}
 			return true;
 		}
 		public static boolean ExperienceAbuseAlert(Player c) {
@@ -102,13 +109,15 @@ public class CheatEngineBlock {
 			Misc.println(""+ c.getDisplayName() +" is trying to use a cheatengine to open the lamps.");
 			Discord.writeServerSyncMessage("[CHEAT ENGINE] "+ c.getDisplayName() +" is using a cheat engine for the lamps!");
 			Discord.writeCheatEngineMessage("[CHEAT ENGINE] " + c.getDisplayName() + " is using a cheat engine for the lamps!");
-			c.setTeleportToX(2086);
-			c.setTeleportToY(4466);
-			c.heightLevel = 0;
-			c.jailEnd = 214700000;
-			c.sendMessage("You have been jailed for using a cheat engine, please speak to staff.");
-			c.sendMessage("If this was a mistake please recall what exactly you did to get here.");
-			c.getPA().closeAllWindows();
+			if(!c.getRights().isOrInherits(Right.ADMINISTRATOR)) {
+				c.setTeleportToX(2086);
+				c.setTeleportToY(4466);
+				c.heightLevel = 0;
+				c.jailEnd = 214700000;
+				c.sendMessage("You have been jailed for using a cheat engine, please speak to staff.");
+				c.sendMessage("If this was a mistake please recall what exactly you did to get here.");
+				c.getPA().closeAllWindows();
+			}
 			return true;
 		}
 
