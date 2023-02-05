f01b44d190afd2c28304cd913d88eec0ae68301c
sgsrocks <seagod6@yahoo.com>
2023-02-05 13:33:04 -0800

    moved the burning skeleton set so the new godsword can be used

--- src/io/exilius/util/discord/Discord.java
+++ src/io/exilius/util/discord/Discord.java
@@ -29,60 +29,86 @@ public class Discord {
      * Write to a channel that contains misc. types of information about player activity.
      */
     public static void writeServerSyncMessage(String message, Object...args) {
-        sendChannelMessage("server-sync", message, args);
+        if(Server.isPublic()) {
+            sendChannelMessage("server-sync", message, args);
+        }
     }
     public static void writeannounceMessage(String message, Object...args) {
-        sendChannelMessage("\uD83D\uDD25-ingame-announcements", message, args);
+        if(Server.isPublic()) {
+            sendChannelMessage("\uD83D\uDD25-ingame-announcements", message, args);
+        }
     }
 
     public static void writeBugMessage(String message, Object...args) {
-        sendChannelMessage("\uD83D\uDD75\uD83C\uDFFBbug-sync", message, args);
+        if(Server.isPublic()) {
+            sendChannelMessage("\uD83D\uDD75\uD83C\uDFFBbug-sync", message, args);
+        }
     }
 
     public static void writetickets(String message, Object...args) {
-        sendChannelMessage("\uD83C\uDFAB-ingame-tickets", message, args);
+        if(Server.isPublic()) {
+            sendChannelMessage("\uD83C\uDFAB-ingame-tickets", message, args);
+        }
     }
 
     public static void writepunishments(String message, Object...args) {
-        sendChannelMessage("ingame-punishments", message, args);
+        if(Server.isPublic()) {
+            sendChannelMessage("ingame-punishments", message, args);
+        }
     }
     public static void writePickupMessage(String message, Object...args) {
-        sendChannelMessage("pickup-sync", message, args);
+        if(Server.isPublic()) {
+            sendChannelMessage("pickup-sync", message, args);
+        }
     }
     public static void raredropMessage(String message, Object...args) {
-        sendChannelMessage("loot", message, args);
+        if(Server.isPublic()) {
+            sendChannelMessage("loot", message, args);
+        }
     }
 
     public static void writeSuggestionMessage(String message, Object...args) {
-        sendChannelMessage("suggestion-sync", message, args);
+        if(Server.isPublic()) {
+            sendChannelMessage("suggestion-sync", message, args);
+        }
     }
 
     public static void writeFoeMessage(String message, Object...args) {
-        writeServerSyncMessage(message, args);
+        if(Server.isPublic()) {
+            writeServerSyncMessage(message, args);
 
-        sendChannelMessage("foe-sync", message, args);
+            sendChannelMessage("foe-sync", message, args);
+        }
     }
 
     public static void writeReferralMessage(String message, Object...args) {
-        writeServerSyncMessage(message, args);
-        sendChannelMessage("referral-sync", message, args);
+        if(Server.isPublic()) {
+            writeServerSyncMessage(message, args);
+            sendChannelMessage("referral-sync", message, args);
+        }
     }
 
     public static void writeCheatEngineMessage(String message, Object...args) {
-        writeServerSyncMessage(message, args);
-        sendChannelMessage("cheat-engine-sync", message, args);
+        if(Server.isPublic()) {
+            writeServerSyncMessage(message, args);
+            sendChannelMessage("cheat-engine-sync", message, args);
+        }
     }
 
     /**
      * Write to a channel that should not be ignored by staff.
      */
     public static void writeAddressSwapMessage(String message, Object...args) {
-        writeServerSyncMessage(message, args);
-        sendChannelMessage("address-swap-sync", message, args);
+        if(Server.isPublic()) {
+            writeServerSyncMessage(message, args);
+            sendChannelMessage("address-swap-sync", message, args);
+        }
     }
     public static void writeServerStatus(String message, Object...args) {
-        writeServerSyncMessage(message, args);
-        sendChannelMessage("server-status", message, args);
+        if(Server.isPublic()) {
+            writeServerSyncMessage(message, args);
+            sendChannelMessage("server-status", message, args);
+        }
     }
     private static void sendChannelMessage(String channelName, String message, Object...args) {
         if (Configuration.DISABLE_DISCORD_MESSAGING || !enabled())
