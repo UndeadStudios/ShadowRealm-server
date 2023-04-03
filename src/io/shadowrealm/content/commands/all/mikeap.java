1a88b3c0a1084de6b9d82204285d4ee5736c1438
0Banned0 <chdaniel929@gmail.com>
2023-04-03 15:32:51 -0300

    Youtubers Link ingame

---
+++ src/io/shadowrealm/content/commands/all/mikeap.java
@@ -0,0 +1,25 @@
+package io.shadowrealm.content.commands.all;
+
+import io.shadowrealm.Configuration;
+import io.shadowrealm.content.commands.Command;
+import io.shadowrealm.content.vote_panel.VotePanelInterface;
+import io.shadowrealm.model.entity.player.Player;
+
+import java.util.Optional;
+
+
+public class mikeap extends Command {
+
+	@Override
+	public void execute(Player c, String commandName, String input) {
+		VotePanelInterface.openInterface(c, true);
+		c.getPA().sendFrame126(Configuration.MIKE_AP, 12000);
+		c.sendMessage("@bla@[@blu@YOUTUBER@bla@] Check out all new content from Mike_ap.");
+	}
+
+	@Override
+	public Optional<String> getDescription() {
+		return Optional.of("Opens a web page where you Check Mikes youtube out");
+	}
+
+}
